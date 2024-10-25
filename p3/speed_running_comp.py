8fdf256a39f058c596e13a3b81e43f5dd15af942
Nifemi456 <niffy2907@gmail.com>
2024-10-25 17:22:08 +0100

    wk3(3) - Fixed leaderboard display bug

--- p3/speed_running_comp.py
+++ p3/speed_running_comp.py
@@ -36,38 +36,58 @@ def display_leaderboard(leaderboard:dict, n:int=3)->None:
     if new_leaderboard.values():
         temp_dict = sorted(new_leaderboard.keys(), key=new_leaderboard.__getitem__)
         for i in range(n):
-            print(f'{i+1} \t {temp_dict[i]} \t {new_leaderboard[temp_dict[i]]}')
+            if i<len(temp_dict):
+                print(f'{i+1}\t{temp_dict[i]}\t{new_leaderboard[temp_dict[i]]}')
     else:
-        print('No times in the Leaderboard')
+        print('Leaderboard is empty.')
 
-def calculate_average_time(leaderboard:dict)->bool:
-    if leaderboard.values():
+def calculate_average_time(leaderboard:dict)->bool or tuple[bool,float]:
+    new_leaderboard = {}
+    for name, value in leaderboard.items():
+        if value:
+            new_leaderboard[name] = value
+    if new_leaderboard.values():
         temp = timedelta(0)
-        for i in leaderboard.values():
+        num = 0
+        for i in new_leaderboard.values():
             if i:
                 temp += i
-        avg_value = temp / len(leaderboard.values())
-        print(f'{avg_value}')
-        return True
+                num += 1
+        avg_value = temp/num
+        return True, avg_value
     else:
-        return False
+        return False, None
 
-leaderboard_1 = init_leaderboard()
-add_player(leaderboard_1, "Frank")
-add_player(leaderboard_1, "Joe")
-add_player(leaderboard_1, "Alan")
-add_player(leaderboard_1, "Steve")
-add_player(leaderboard_1, "Chris")
-display_leaderboard(leaderboard_1)
-# print(leaderboard_1)
-add_run(leaderboard_1, "Frank", timedelta(hours=1))
-add_run(leaderboard_1, "Joe", timedelta(hours=2))
-add_run(leaderboard_1, "Alan", timedelta(hours=3))
-add_run(leaderboard_1, "Alan", timedelta(minutes=57))
-add_run(leaderboard_1, "Steve", timedelta(minutes=49))
-add_run(leaderboard_1, "Chris", timedelta(minutes=69))
-clear_score(leaderboard_1, "Steve")
-# print(leaderboard_1)
-display_leaderboard(leaderboard_1)
-print(f"Average best time: {calculate_average_time(leaderboard_1)}")
+# leaderboard_1 = init_leaderboard()
+# add_player(leaderboard_1, "Frank")
+# add_player(leaderboard_1, "Joe")
+# add_player(leaderboard_1, "Alan")
+# add_player(leaderboard_1, "Steve")
+# add_player(leaderboard_1, "Chris")
+# display_leaderboard(leaderboard_1)
+# # print(leaderboard_1)
+# add_run(leaderboard_1, "Frank", timedelta(hours=1))
+# add_run(leaderboard_1, "Joe", timedelta(hours=2))
+# add_run(leaderboard_1, "Alan", timedelta(hours=3))
+# add_run(leaderboard_1, "Alan", timedelta(minutes=57))
+# add_run(leaderboard_1, "Steve", timedelta(minutes=49))
+# add_run(leaderboard_1, "Chris", timedelta(minutes=69))
+# clear_score(leaderboard_1, "Steve")
+# # print(leaderboard_1)
+# display_leaderboard(leaderboard_1)
+# print(f"Average best time: {calculate_average_time(leaderboard_1)[1]}")
 
+leaderboard = init_leaderboard()
+display_leaderboard(leaderboard, n=2)
+add_player(leaderboard, "Alice")
+display_leaderboard(leaderboard, n=2)
+add_player(leaderboard, "Bob")
+display_leaderboard(leaderboard, n=2)
+add_player(leaderboard, "Charlie")
+display_leaderboard(leaderboard, n=2)
+add_run(leaderboard, "Alice", timedelta(seconds=67))
+display_leaderboard(leaderboard, n=2)
+add_run(leaderboard, "Bob", timedelta(seconds=53))
+display_leaderboard(leaderboard, n=2)
+add_run(leaderboard, "Charlie", timedelta(seconds=63))
+display_leaderboard(leaderboard, n=2)
